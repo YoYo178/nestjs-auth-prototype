99450639cca6573161bb808ffae0910060db903d
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-11-26 17:53:28 +0530

    feat(cache): setup basic caching

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    CacheModule.register({ isGlobal: true }),
+
     PrismaModule,
     AuthModule,
   ],
