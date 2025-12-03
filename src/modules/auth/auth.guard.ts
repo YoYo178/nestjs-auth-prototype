2b729184a2d3e01d89465d52a947af61dd1820a2
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-12-03 17:01:29 +0530

    chore(auth): update auth guard, type augmentation
    if any exception is thrown, the client's cookie is cleared

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -1,26 +1,34 @@
 import { CanActivate, ExecutionContext, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private readonly authService: AuthService) {}
-    
+    constructor(private readonly authService: AuthService) { }
+
     async canActivate(ctx: ExecutionContext): Promise<boolean> {
         const request = ctx.switchToHttp().getRequest<Request>();
         const sessionId = request.cookies['sid'];
 
-        if(!sessionId)
-            throw new UnauthorizedException();
+        try {
+            if (!sessionId)
+                throw new UnauthorizedException();
 
-        const user = await this.authService.validateSessionId(sessionId);
+            const user = await this.authService.validateSessionId(sessionId);
 
-        // Highly unlikely again, but just in case...
-        if(!user)
-            throw new NotFoundException('User not found');
+            // Highly unlikely again, but just in case...
+            if (!user)
+                throw new NotFoundException('User not found');
 
-        request['user'] = user;
+            request['user'] = user;
 
-        return true;
+            return true;
+        } catch (err) {
+            // If anything bad happened due to the cookie, we most probably don't need it to stay
+            const response = ctx.switchToHttp().getResponse<Response>();
+            response.clearCookie('sid');
+
+            throw err;
+        }
     }
 }
