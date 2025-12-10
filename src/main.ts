cdae26cf64ff0f77de75d61c3d574eded6f5c578
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-12-10 15:25:18 +0530

    feat(log): add logging interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
